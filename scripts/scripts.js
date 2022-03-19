3bb3434fd6823be50eff979526e8f277d3813163
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-03-19 15:39:48 +0100

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,21 @@
 const navIcon = document.querySelector('nav div img');
 const navSmScreen = document.querySelector('.nav-sm-screen');
+const navSmScreenNavIcon = document.querySelector('.nav-sm-screen div img')
 const navToggle = document.querySelector('.nav-toggle');
 const navToggleImage = document.querySelector('.nav-toggle img');
 
 navIcon.addEventListener('click', () => {
     navToggle.style.transform = 'translateY(0)';
+    navIcon.classList.toggle('display');
 })
 
 navSmScreen.addEventListener('click', () => {
     navToggle.style.transform = 'translateY(0)';
+    navSmScreenNavIcon.classList.toggle('display');
 })
 
 navToggleImage.addEventListener('click', () => {
     navToggle.style.transform = 'translateY(-100%)';
-})
-console.log(navIcon);
+    navIcon.classList.remove('display');
+    navSmScreenNavIcon.classList.remove('display');
+}) 
