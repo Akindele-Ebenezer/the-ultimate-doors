7d6d2b4b381750b4be93580c9bdc9d4840dd6f68
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-03-19 22:24:40 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
 
-    $conn = mysqli_connect('localhost', 'root', 'ebenkeyz1*', 'the_ultimate_doors');
+    $conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b8e6e75d73b46e', 'f9a827b1', 'heroku_64827b4daa6e035');
 
 ?>
